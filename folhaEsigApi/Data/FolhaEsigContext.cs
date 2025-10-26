95d554194c79c74238ce28c10f9f79ca205ec888
Ana Paula <ana.paula.ufrn.ti@gmail.com>
2025-10-26 17:36:18 -0300

    chore: apresentando o ocntexto do banco e configurando o ambiente

---
+++ folhaEsigApi/Data/FolhaEsigContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FolhaEsigAPI.Models;
+
+namespace FolhaEsigAPI.Data
+{
+    public class FolhaEsigContext : DbContext
+    {
+        public FolhaEsigContext(DbContextOptions<FolhaEsigContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Cargo> Cargos { get; set; }
+        public DbSet<PessoaSalario> PessoaSalarios { get; set; }
+    }
+}
--- folhaEsigApi/Program.cs
+++ folhaEsigApi/Program.cs
@@ -1,23 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using FolhaEsigAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<FolhaEsigContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 33)) 
+    )
+);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
